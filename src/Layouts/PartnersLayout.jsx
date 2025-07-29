fa360e4bfa41e38e87e48b81c699c9313ccce82e
PiyushSharma8279 <piyush.vrwebconsulting@gmail.com>
2025-07-29 15:10:57 +0530

    import file

--- src/Layouts/PartnersLayout.jsx
+++ src/Layouts/PartnersLayout.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Footer from '../components/footer/footer'
+
 import Header from '../components/header/header'
+import Footer from '../components/footer/Footer'
 
 function PartnersLayout() {
     return (
