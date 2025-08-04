8cc8ac3dc25d708ee019d932a8df294dd7805330
PiyushSharma8279 <piyush.vrwebconsulting@gmail.com>
2025-08-04 17:35:08 +0530

    blogs page

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Header from '../../components/header/Header'
 import BlogImg from "../../assets/blog-masthead-desktop.png"
 import Everthing from './EverthingCategory/Everthing'
-import Footer from '../../components/footer/Footer'
+import BlogImgMobile from "../../assets/blog-home-mobile.png"
+
+
 
 function Blogs() {
     return (
@@ -11,33 +13,29 @@ function Blogs() {
             <div>
                 <div className='bg-gradient-to-b  bg-[#275c5e] to-[#458d92] mt-13 flex flex-col items-center w-full  '>
                     <div className='mt-[20px] h-[500px] '>
-                        <img src={BlogImg} alt="" className=' h-[380px]' />
+                        <img src={BlogImg} alt="" className=' h-[380px] hidden sm:hidden md:block lg:block' />
+                        <img src={BlogImgMobile} alt="" className=' h-[400px] w-full lg:hidden md:hidden' />
                         <div className=' flex flex-col justify-center items-center text-white'>
-                            <h1 className=' text-[2.5rem] font-bold'>lambda</h1>
-                            <p className=' text-[1.125rem] font-medium'>Learn about everything we do at Blinkit</p>
+                            <h1 className=' lg:text-[2.5rem] md:text-[2.5rem] text-[2rem] font-bold'>lambda</h1>
+                            <p className=' lg:text-[1.125rem] md:text-[1.125rem] text-[1rem] font-medium'>Learn about everything we do at Blinkit</p>
                         </div>
                     </div>
                 </div>
-                <div>
-
-                </div>
-                <div className=' flex justify-center'>
-
-
-                </div>
-                <div className=' flex flex-col items-center justify-center'>
+                {/* <div className=' flex flex-col items-center justify-center'>
                     <div className='w-[80%]'>
                         <div className=' flex flex-col justify-center items-center'>
                             <Everthing />
                         </div>
                     </div>
+                </div> */}
+                <Everthing/>
+
+
+
+                <div className=' text-center my-10'>
+                    <p className=' text-[18px]'>You have reached the end of our appetite.</p>
+                    <p className=' py-4 text-gray-300'>© Grofers Pvt Ltd. All rights reserved.</p>
                 </div>
-
-
-
-<div>
-    <Footer/>
-</div>
             </div>
 
 
--- src/Pages/Blogs/EverthingCategory/Everthing.jsx
+++ src/Pages/Blogs/EverthingCategory/Everthing.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
 
 function Everthing() {
     const data = [
@@ -14,9 +13,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2024-07/blog-blinkit-web_3.png?itok=GGWCJbbO",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-          {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2024-03/Blog%20Header-1920x553%20%281%29.png?itok=p0lCJiot",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -26,9 +25,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-05/Print_web%20%281%29.jpg?itok=ysidjLTI",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-          {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-05/Frame%20228_0.png?itok=vIBlIjf3",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -38,9 +37,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-03/in-focus_Sumanth%20Reddy_web.png?itok=lsH2zGOs",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-          {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-03/in-focus_sancheeta-lambda.png?itok=4plNYuO1",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -50,9 +49,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-02/Web.png?itok=a0Mh9da7",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-         {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2024-07/data%20warehouse%20journey%20dbt.png?itok=3AlcU6Es",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -62,9 +61,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2024-07/blog-blinkit-web_3.png?itok=GGWCJbbO",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-          {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2024-03/Blog%20Header-1920x553%20%281%29.png?itok=p0lCJiot",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -74,9 +73,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-05/Print_web%20%281%29.jpg?itok=ysidjLTI",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-          {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-05/Frame%20228_0.png?itok=vIBlIjf3",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -86,9 +85,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-03/in-focus_Sumanth%20Reddy_web.png?itok=lsH2zGOs",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-          {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-03/in-focus_sancheeta-lambda.png?itok=4plNYuO1",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -98,9 +97,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-02/Web.png?itok=a0Mh9da7",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-         {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2024-07/data%20warehouse%20journey%20dbt.png?itok=3AlcU6Es",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -110,9 +109,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2024-07/blog-blinkit-web_3.png?itok=GGWCJbbO",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-          {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2024-03/Blog%20Header-1920x553%20%281%29.png?itok=p0lCJiot",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -122,9 +121,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-05/Print_web%20%281%29.jpg?itok=ysidjLTI",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-          {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-05/Frame%20228_0.png?itok=vIBlIjf3",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -134,9 +133,9 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-03/in-focus_Sumanth%20Reddy_web.png?itok=lsH2zGOs",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-          {
+        {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-03/in-focus_sancheeta-lambda.png?itok=4plNYuO1",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "Data Warehouse Journey With dbt",
@@ -146,57 +145,66 @@ function Everthing() {
             img: "https://blinkit.com/blog/sites/default/files/styles/banner_thumbnail/public/2023-02/Web.png?itok=a0Mh9da7",
             info: "Team Blinkit | July 16, 2024 | 6 mins read",
             title: "VBR at WWDC 24",
-            content: "When I told Tim Cook about the 10 minute iPhone delivery by Blinkit his reaction was, “that’s crazy”, and all I could think"
+            content: "Nitesh Jemni is the first UI/UX designer of Grofers. He is currently working as the UI/UX Design Lead."
         },
-    
+
     ]
 
     const [filteredData, setFilteredData] = useState(data);
 
-  const filterByTechnology = () => {
-    const result = data.filter(item => item.title === "VBR at WWDC 24");
-    setFilteredData(result);
-  };
-  const filterByCulture = () => {
-    const result = data.filter(item => item.title === "Data Warehouse Journey With dbt");
-    setFilteredData(result);
-  };
+    const filterByTechnology = () => {
+        const result = data.filter(item => item.title === "VBR at WWDC 24");
+        setFilteredData(result);
+    };
+    const filterByCulture = () => {
+        const result = data.filter(item => item.title === "Data Warehouse Journey With dbt");
+        setFilteredData(result);
+    };
 
-  const resetFilter = () => {
-    setFilteredData(data);
-  };
+    const resetFilter = () => {
+        setFilteredData(data);
+    };
 
-  return (
-    <>
-      <div>
-        <div className='flex justify-evenly text-[22px] my-[20px] gap-5'>
-          <a className='cursor-pointer hover:underline decoration-blue-400 underline-offset-15' onClick={resetFilter}>Everything</a>
-          <a className='cursor-pointer hover:underline decoration-blue-400 underline-offset-15' onClick={filterByTechnology}>Technology</a>
-          <a className='cursor-pointer hover:underline decoration-blue-400 underline-offset-15' onClick={filterByCulture}>Culture</a>
-          <a className='cursor-pointer hover:underline decoration-blue-400 underline-offset-15' onClick={filterByTechnology}>Humans of VBR</a>
-          <a className='cursor-pointer hover:underline decoration-blue-400 underline-offset-15' onClick={filterByCulture}>Newsroom</a>
-          <a className='cursor-pointer hover:underline decoration-blue-400 underline-offset-15' onClick={filterByTechnology}>Sustainability</a>
-        </div>
-      </div>
-
-      <div className='flex flex-wrap w-full'>
-        {filteredData.map((item, index) => (
-          <div className='w-1/2 my-[40px]' key={index}>
-            <div className='overflow-hidden relative w-full h-[300px]'>
-              <img
-                src={item.img}
-                alt={item.title}
-                className='h-full object-cover scale-100 hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer'
-              />
+    return (
+        <>
+            
+            <div className="flex justify-center px-4">
+                <div className="w-full lg:w-[80%] max-w-[1200px]">
+                    <div className="flex flex-wrap justify-evenly  text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] my-6 text-center">
+                        <a className="cursor-pointer hover:underline decoration-blue-400 underline-offset-4 transition-all" onClick={resetFilter}>Everything</a>
+                        <a className="cursor-pointer hover:underline decoration-blue-400 underline-offset-4 transition-all" onClick={filterByTechnology}>Technology</a>
+                        <a className="cursor-pointer hover:underline decoration-blue-400 underline-offset-4 transition-all" onClick={filterByCulture}>Culture</a>
+                        <a className="cursor-pointer hover:underline decoration-blue-400 underline-offset-4 transition-all" onClick={filterByTechnology}>Humans of VBR</a>
+                        <a className="cursor-pointer hover:underline decoration-blue-400 underline-offset-4 transition-all" onClick={filterByCulture}>Newsroom</a>
+                        <a className="cursor-pointer hover:underline decoration-blue-400 underline-offset-4 transition-all" onClick={filterByTechnology}>Sustainability</a>
+                    </div>
+                </div>
             </div>
-            <p className='text-[.75rem] py-[20px] text-[#666]'>{item.info}</p>
-            <h2 className='text-[1.5rem] py-[10px] font-normal text-[#1f1f1f] cursor-pointer'>{item.title}</h2>
-            <p className='text-[1rem] py-[20px] text-[#333333]'>{item.content}</p>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+
+           
+            <div className="flex justify-center px-4 mt-4">
+                <div className="w-full lg:w-[80%] max-w-[1200px] flex flex-wrap justify-center gap-8">
+                    {filteredData.map((item, index) => (
+                        <div key={index} className="w-full sm:w-[90%] md:w-[48%]">
+                            <div className="overflow-hidden relative w-full h-[300px] rounded-lg">
+                                <img
+                                    src={item.img}
+                                    alt={item.title}
+                                    className="w-full h-full object-cover scale-100 hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer"
+                                />
+                            </div>
+                            <p className="text-sm py-4 text-[#666]">{item.info}</p>
+                            <h2 className="text-xl py-2 font-semibold text-[#1f1f1f] cursor-pointer">
+                                {item.title}
+                            </h2>
+                            <p className="text-base py-4 text-[#333]">{item.content}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+        </>
+    );
 }
 
 export default Everthing;
