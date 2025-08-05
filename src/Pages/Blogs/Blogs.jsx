27881f377bff1608c2d12ac653e3de058848b12f
PiyushSharma8279 <piyush.vrwebconsulting@gmail.com>
2025-08-05 11:16:36 +0530

    updating blog

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -12,25 +12,16 @@ function Blogs() {
             <Header />
             <div>
                 <div className='bg-gradient-to-b  bg-[#275c5e] to-[#458d92] mt-13 flex flex-col items-center w-full  '>
-                    <div className='mt-[20px] h-[500px] '>
-                        <img src={BlogImg} alt="" className=' h-[380px] hidden sm:hidden md:block lg:block' />
-                        <img src={BlogImgMobile} alt="" className=' h-[400px] w-full lg:hidden md:hidden' />
+                    <div className='mt-[20px] h-[450px] '>
+                        <img src={BlogImg} alt="" className=' h-[320px] hidden sm:hidden md:block lg:block' />
+                        <img src={BlogImgMobile} alt="" className=' h-[350px] w-full lg:hidden md:hidden' />
                         <div className=' flex flex-col justify-center items-center text-white'>
                             <h1 className=' lg:text-[2.5rem] md:text-[2.5rem] text-[2rem] font-bold'>lambda</h1>
                             <p className=' lg:text-[1.125rem] md:text-[1.125rem] text-[1rem] font-medium'>Learn about everything we do at Blinkit</p>
                         </div>
                     </div>
                 </div>
-                {/* <div className=' flex flex-col items-center justify-center'>
-                    <div className='w-[80%]'>
-                        <div className=' flex flex-col justify-center items-center'>
-                            <Everthing />
-                        </div>
-                    </div>
-                </div> */}
                 <Everthing/>
-
-
 
                 <div className=' text-center my-10'>
                     <p className=' text-[18px]'>You have reached the end of our appetite.</p>
